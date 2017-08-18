d63f2a868f44a588b269caba8af26b5858ed5396
xujiajun <me@xujiajun.cn>
2017-08-18 17:36:35 +0800

    add node status show

--- src/FrontBundle/Controller/HomeController.php
+++ src/FrontBundle/Controller/HomeController.php
@@ -8,6 +8,21 @@ class HomeController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('home/index.html.twig');
+        $ipsReal = $this->get('redis')->hGetAll('ips');
+        $ipsAll = $this->get('redis')->hGetAll('ips_all');
+        $services = $this->get('redis')->hGetAll('services');
+        $serviceStatus = $this->get('redis')->hGetAll('s_status');
+
+        $data = [];
+
+        foreach ($ipsAll as $ip => $weight) {
+            $data[$ip]['status'] = $serviceStatus[$ip];
+            $data[$ip]['weight'] = $weight;
+            $data[$ip]['serviceName'] = $services[$ip];
+        }
+
+        return $this->render('home/index.html.twig',[
+            'data' => $data
+        ]);
     }
 }
--- web/bootstrap.php
+++ web/bootstrap.php
@@ -2,3 +2,5 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 define('__BASEDIR__', __DIR__ . '/..');
+
+define('APP_VERSION', 'v1.3.2');
