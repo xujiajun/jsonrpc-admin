ee4463b29084d3547b2425c36d9b2c1d0c067567
xujiajun <me@xujiajun.cn>
2017-08-21 19:41:51 +0800

    add modifyWeightAction

--- src/FrontBundle/Controller/HomeController.php
+++ src/FrontBundle/Controller/HomeController.php
@@ -3,13 +3,15 @@
 namespace TastPHP\FrontBundle\Controller;
 
 use TastPHP\Common\Controller;
+use TastPHP\Framework\Http\Request;
 
 class HomeController extends Controller
 {
     public function indexAction()
     {
-        $ipsReal = $this->get('redis')->hGetAll('ips');
+//        $ipsReal = $this->get('redis')->hGetAll('ips');
         $ipsAll = $this->get('redis')->hGetAll('ips_all');
+
         $services = $this->get('redis')->hGetAll('services');
         $serviceStatus = $this->get('redis')->hGetAll('s_status');
 
@@ -21,8 +23,23 @@ class HomeController extends Controller
             $data[$ip]['serviceName'] = $services[$ip];
         }
 
-        return $this->render('home/index.html.twig',[
+        return $this->render('home/index.html.twig', [
             'data' => $data
         ]);
+    }
+
+    public function modifyWeightAction(Request $request)
+    {
+        if (empty($request->request->get("weight")) || empty($request->request->get("ip"))) {
+            echo -1;
+        }
+
+        $weight = $request->request->get("weight");
+        $ip = $request->request->get("ip");
+
+        $this->get('redis')->hSet('ips_all', $ip, $weight);
+        $this->get('redis')->hSet('ips', $ip, $weight);
+
+        echo 1;
     }
 }
